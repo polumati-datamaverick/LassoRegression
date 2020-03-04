79b3e45af364d70253c8f3a2202a3b874d5b9eff
satish-datamaverick <satishkumarpolumati@Satishs-MacBook-Pro.local>
2020-03-04 17:52:45 -0500

    corrected testing script

--- packages/regression_model/test/test_predict.py
+++ packages/regression_model/test/test_predict.py
@@ -7,7 +7,7 @@ from regression_model import config
 
 def test_make_single_prediction():
 
-    test_data = load_dataset(file_name=config.TESTING_DATA_FILE)
+    test_data = load_dataset(file_name='test.csv')
     single_test_json = test_data[0:1]
     output = make_prediction(input_data=single_test_json)
 
@@ -18,7 +18,7 @@ def test_make_single_prediction():
 
 def test_make_multiple_predictions():
 
-    test_data = load_dataset(file_name=config.TESTING_DATA_FILE)
+    test_data = load_dataset(file_name='test.csv')
     original_data_length = len(test_data)
     multiple_test_json = test_data
     output = make_prediction(input_data=multiple_test_json)
